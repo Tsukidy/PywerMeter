d08c169bba9ccca05947d0251d967949bb908a27
Dylan <popedyit@gmail.com>
2025-11-12 17:36:38 -0500

    Datalog bug fix

--- pywerMeter.py
+++ pywerMeter.py
@@ -6,6 +6,7 @@ print(excelHelper.testFunction())
 
 if __name__ == "__main__":
     dev = serialComm.SerialDevice(port="COM9")
+    dataLog = ""
     for _ in range(10):
         print("Attempting to read serial data.")
         seialData = dev.query()
