10ccaf4f7ab0f8450d9faae92db0071baa686207
Dylan <popedyit@gmail.com>
2025-11-12 17:16:26 -0500

    Fixing serial connection

--- pywerMeter.py
+++ pywerMeter.py
@@ -5,6 +5,7 @@ print(serialComm.testFunction())
 print(excelHelper.testFunction())
 
 if __name__ == "__main__":
-    print("Attempting to read serial data.")
-    seialData = serialComm.query_device()
-    print(f"Received serial data: {seialData}")
+    while True:
+        print("Attempting to read serial data.")
+        seialData = serialComm.query_device()
+        print(f"Received serial data: {seialData}")
