f290fbc51de4c70e2dee107df8f1d3d28256c739
Dylan <popedyit@gmail.com>
2025-11-12 17:41:51 -0500

    Making this operate off x minutes

--- pywerMeter.py
+++ pywerMeter.py
@@ -1,4 +1,5 @@
 from pywerHelper import serialComm, excelHelper
+import time
 
 print("Testing pywerHelper package")
 print(serialComm.testFunction())
@@ -7,7 +8,10 @@ print(excelHelper.testFunction())
 if __name__ == "__main__":
     dev = serialComm.SerialDevice(port="COM9")
     dataLog = ""
-    for _ in range(10):
+    minutes = 1
+    end_time = time.time() + minutes * 60
+    #for _ in range(10):
+    while time.time() < end_time:
         print("Attempting to read serial data.")
         serialData = dev.query()
         dataLog += serialData + "\n"
