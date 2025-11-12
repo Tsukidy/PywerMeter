e8e3343a2cccd582b9cce5ed389cce3008bd7640
Dylan <popedyit@gmail.com>
2025-11-12 13:51:49 -0500

    Added a function to list current serial ports on the unit. When the script is called.

--- pywerHelper/serialComm.py
+++ pywerHelper/serialComm.py
@@ -4,6 +4,11 @@ import serial
 def testFunction():
     return "This is a test function from serialComm.py"
 
+def returnSerialPorts():
+    import serial.tools.list_ports
+    ports = serial.tools.list_ports.comports()
+    return [port.device for port in ports]
+
 def recieve_data():
     try:
         while True:
@@ -17,3 +22,8 @@ def recieve_data():
         Print("Serial communication terminated by user.")
     finally:
         exit(0)
+
+if __name__ == "__main__":
+    print ("Returning available serial ports:")
+    returned_ports = returnSerialPorts()
+    print(returned_ports)
