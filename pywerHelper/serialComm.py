b58f9ca10f9792351f1eb3220dcf538c931936fd
Dylan <popedyit@gmail.com>
2025-11-12 17:10:56 -0500

    Fixing serial connection

--- pywerHelper/serialComm.py
+++ pywerHelper/serialComm.py
@@ -11,6 +11,7 @@ def returnSerialPorts():
         print(f"{port.device}: {port.description}")
 
 def query_device(
+    command=b'?',
     port="COM9",
     baudrate=9600,
     bytesize=serial.EIGHTBITS,
@@ -18,32 +19,6 @@ def query_device(
     timeout=1
 ):
     with serial.Serial(port=port, baudrate=baudrate, bytesize=bytesize, stopbits=stopbits, timeout=timeout) as ser:
-        ser.write(b'?')
+        ser.write(command)
         response = ser.read(64)  # Adjust size as needed
         print("Response (hex):", response.hex())
-
-def recieve_data(port="COM9", baudrate=9600, timeout=1, stopbits=1, bytesize=8):
-    command = '?'
-    encodedCommand = command.encode('ascii')
-    try:
-        while True:
-            # Open serial connection
-            with serial.Serial(port, baudrate, timeout) as ser:
-                # Send a ? to the device to request data
-                ser.write(encodedCommand)
-                # Read a line from the serial port
-                line = ser.readline()
-                text = line.decode('utf-8').rstrip()
-                if line:
-                    print(f"Received: {text}")
-                    print(f"Debug output: {line}")
-                    sleep(1)  # Pause for a second before next read
-    except KeyboardInterrupt:
-        Print("Serial communication terminated by user.")
-    finally:
-        exit(0)
-
-if __name__ == "__main__":
-    print ("Returning available serial ports:")
-    returned_ports = returnSerialPorts()
-    print(returned_ports)
