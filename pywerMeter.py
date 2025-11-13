8280204cf4dfcd6b1ee98cf2fefd54b066469a42
Dylan <popedyit@gmail.com>
2025-11-13 12:03:45 -0500

    Reducing time that the tests runs for debug

--- pywerMeter.py
+++ pywerMeter.py
@@ -8,7 +8,7 @@ print(excelHelper.testFunction())
 if __name__ == "__main__":
     dev = serialComm.SerialDevice(port="COM9", baudrate=38400, timeout=0.5)
     dataLog = ""
-    minutes = 1
+    minutes = 0.25
     end_time = time.time() + minutes * 60
     while time.time() < end_time:
         print("Attempting to read serial data.")
