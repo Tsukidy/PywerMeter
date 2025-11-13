37e34550014518698664d2be748a8a274c23cee2
Dylan <popedyit@gmail.com>
2025-11-13 12:12:33 -0500

    Reducing time that the tests runs for debug

--- pywerHelper/serialComm.py
+++ pywerHelper/serialComm.py
@@ -1,29 +1,11 @@
 # Python script for handling serial communication wtih a device.
 import serial
 
-def testFunction():
-    return "This is a test function from serialComm.py"
-
 def returnSerialPorts():
     import serial.tools.list_ports
     ports = serial.tools.list_ports.comports()
     for port in ports:
         print(f"{port.device}: {port.description}")
-
-def query_device(
-    command=b'?',
-    port="COM9",
-    baudrate=9600,
-    bytesize=serial.EIGHTBITS,
-    stopbits=serial.STOPBITS_ONE,
-    timeout=1
-):
-    with serial.Serial(port=port, baudrate=baudrate, bytesize=bytesize, stopbits=stopbits, timeout=timeout) as ser:
-        ser.write(command)
-        response = ser.read(64)  # Adjust size as needed
-        hex_str = response.hex()
-        spaced_hex = ' '.join([hex_str[i:i+2] for i in range(0, len(hex_str), 2)])
-        return spaced_hex
 
 class SerialDevice:
     def __init__(
@@ -44,7 +26,7 @@ class SerialDevice:
 
     def query(
         self,
-        command=b'?'
+        command=b'?MPOW'
     ):
         self.ser.write(command)
         response = self.ser.read(64)
@@ -56,3 +38,5 @@ class SerialDevice:
     def close(self):
         self.ser.close()
 
+if __name__ == "__main__":
+    returnSerialPorts()
